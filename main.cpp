c06d53994fbcd58e6b6d34b8bb5c76ed04f262ee
Danylo Halaiko <dan.galaiko@gmail.com>
2020-02-24 16:43:39 +0200

    I wrote new massage. It was fun))

--- main.cpp
+++ main.cpp
@@ -3,5 +3,6 @@ using namespace std;
 int main() {
     cout << "Lesik first commit!!" << endl;
     cout << "I am not watching porn!!!"<< endl;
+    cout << "You are not sincere!!" << endl;
     return 0;
 }
