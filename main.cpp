42fa41e91ada4fe80cb9c465833de38a1a2c7e16
Danylo Halaiko <dan.galaiko@gmail.com>
2020-02-24 16:39:48 +0200

    Second

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
 int main() {
-    cout << "Lesik first commit!!";
+    cout << "Lesik first commit!!" << endl;
+    cout << "I am not watching porn!!!"<< endl;
     return 0;
 }
